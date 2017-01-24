a7bad5fedfda3a1d49f090c4410f6d489d1683d5
bkwaku <bkwaku@rocketmail.com>
2017-01-24 21:09:06 +0000

    Separated api into versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  
+  scope module: 'api' do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  #from Now on routes go like this: 
+  # 1. /v1/users = to access all users
+  # 2. /v1/users/1 = to access one user
+  #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124124526) do
+ActiveRecord::Schema.define(version: 20170124130858) do
+
+  create_table "meals", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "picture_path"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,14 +28,6 @@ ActiveRecord::Schema.define(version: 20170124124526) do
     t.integer  "sex"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    end
-
-  create_table "meals", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "picture_path"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
   end
 
 end
