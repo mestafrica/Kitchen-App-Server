89d27feb8fd74febf8c417e99b5417800850dd22
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-01-24 19:28:48 +0000

    Scaffold user model
    This commit scaffolds the user model, it also adds the RSpec to the
    rails application

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170124124526) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "nationality"
+    t.integer  "sex"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
