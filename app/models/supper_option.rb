a9897defa2eb44d837696cf4b4ec728ecf7efb50
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-04-23 21:39:30 +0000

    Refactor MenuController and the Menu model for readability and simplicity

--- app/models/supper_option.rb
+++ app/models/supper_option.rb
@@ -1,4 +1,12 @@
 class SupperOption < ApplicationRecord
   belongs_to :meal
   validates :serving_date, :meal, presence: true
+
+  def self.create_all_by_serving_date!(serving_date, supper_choices)
+    return if supper_choices.nil?
+
+    supper_choices.each do |supper_choice|
+      create!(serving_date: serving_date, meal_id: supper_choice)
+    end
+  end
 end
