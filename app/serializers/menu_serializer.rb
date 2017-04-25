fee79161383fdac09c92f37a7442c9496d8ce3ee
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-04-25 13:57:55 +0000

    Remove id from BreakfastOption, LunchOption and SupperOption in Menu Serializer

--- app/serializers/menu_serializer.rb
+++ app/serializers/menu_serializer.rb
@@ -3,4 +3,20 @@ class MenuSerializer < ActiveModel::Serializer
   has_many :breakfast_options
   has_many :lunch_options
   has_many :supper_options
+
+
+  class BreakfastOptionSerializer < ActiveModel::Serializer
+    attributes :serving_date
+    has_one :meal
+  end
+
+  class LunchOptionSerializer < ActiveModel::Serializer
+    attributes :serving_date
+    has_one :meal
+  end
+
+  class SupperOptionSerializer < ActiveModel::Serializer
+    attributes :serving_date
+    has_one :meal
+  end
 end
